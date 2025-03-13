73313525e86cae2491e041e627d06bbcff9040cd
Indian1999 <haszab99@gmail.com>
2025-03-13 16:10:29 +0100

    dice_matrix

--- 2.félév/matrix_list_comprehension.py
+++ 2.félév/matrix_list_comprehension.py
@@ -31,8 +31,36 @@ for i in range(len(matrix)): # sorok számáig megy
 print("A mátrix elemeinek az összege:", összeg)
 
 # 1. feladat: Írjuk ki a legkisebb számot a mátrixban
+minimum = matrix[0][0]
+for i in range(len(matrix)):
+    for j in range(len(matrix[i])):
+        if matrix[i][j] < minimum:
+            minimum = matrix[i][j]
+print("A legkisebb szám a mátrixban:", minimum)
 
 # 2. feladat: Adjuk meg, hogy hány negatív szám van a mátrixban
+neg_counter = 0
+for i in range(len(matrix)):
+    for j in range(len(matrix[i])):
+        if matrix[i][j] < 0:
+            neg_counter += 1
+print("A negatív számok darabszáma:", neg_counter)
     
+# List comprehension
+
+lista = [0 for i in range(10)]
+print(lista)
+lista = [i for i in range(10)]
+print(lista)
+lista = [i**2 -4*i + 2 for i in range(10)]
+print(lista)
+lista = [[] for i in range(10)]
+print(lista)
+lista = [[j*i for j in range(3)] for i in range(3)]
+print(lista)
+
+dice_matrix = [[i + j for j in range(1,7)] for i in range(1, 7)]
+for row in dice_matrix:
+    print(row)
 
     
