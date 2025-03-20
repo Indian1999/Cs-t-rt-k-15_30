a1d5dcd426ef5f84ba0443eb7068ebd2564d0ed3
Indian1999 <haszab99@gmail.com>
2025-03-20 16:45:12 +0100

    is_palindrome improved

--- 2.félév/MyPackage/functions.py
+++ 2.félév/MyPackage/functions.py
@@ -1,8 +1,17 @@
+from .data import special_chars
+
 def print_rectangle(a, b):
     for i in range(a):
         print("# "*b)
         
-def is_palindrome(string):
+def is_palindrome(string, exact_characters = True):
+    if not exact_characters:
+        new_string = ""
+        for char in string:
+            if char not in special_chars:
+                new_string += char
+        string = new_string.lower().replace(" ", "")
+    
     if len(string) == 1 or len(string) == 0:
         return True
     else:
--- 2.félév/saját_modulok.py
+++ 2.félév/saját_modulok.py
@@ -13,11 +13,6 @@ print("Az 5 egység sugarú kör területe:", MyPackage.pi * 5**2)
 # Döntsük el egy szövegről, hogy palindróm-e
 
 szöveg = "In+dul,? a ,gö:rög- aludni."
-uj_szöveg = ""
-for char in szöveg:
-    if char not in MyPackage.special_chars:
-        uj_szöveg += char
-szöveg = uj_szöveg.lower().replace(" ", "")
-print(szöveg)
 
 print(MyPackage.is_palindrome(szöveg))
+print(MyPackage.is_palindrome(szöveg, False))
