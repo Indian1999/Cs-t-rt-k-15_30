dcb65b9c22c2694b69c4f8e4115239b37d4f2811
Indian1999 <haszab99@gmail.com>
2025-03-20 16:59:37 +0100

    datetime

--- 2.félév/saját_modulok.py
+++ 2.félév/saját_modulok.py
@@ -16,3 +16,12 @@ szöveg = "In+dul,? a ,gö:rög- aludni."
 
 print(MyPackage.is_palindrome(szöveg))
 print(MyPackage.is_palindrome(szöveg, False))
+
+import datetime
+
+# datetime modul datetime osztályának a now() függvényét hívjuk
+print(datetime.datetime.now()) 
+print(datetime.date.today())
+print(datetime.date.today() - datetime.timedelta(days=200000))
+
+print(datetime.date.today() - datetime.date(year=1, month=1, day=1))
