f3f4083dedf3912d0267e458752024ea514dfbb6
Indian1999 <haszab99@gmail.com>
2025-03-27 15:55:58 +0100

    print_board()

--- 2.félév/sudoku_solver.py
+++ 2.félév/sudoku_solver.py
@@ -9,3 +9,17 @@ board = [
     [1, 2, 0, 0, 0, 7, 4, 0, 0],
     [0, 4, 9, 2, 0, 6, 0, 0, 7]
 ]
+
+def print_board():
+    for i in range(len(board)):
+        if i % 3 == 0 and i != 0:
+            print("- " * 12)
+        output = ""
+        for j in range(len(board[i])):
+            if j % 3 == 0 and j != 0:
+                output += " | " #alt gr + w -> |
+            output += str(board[i][j]) + " "
+        print(output)
+        
+        
+print_board()
