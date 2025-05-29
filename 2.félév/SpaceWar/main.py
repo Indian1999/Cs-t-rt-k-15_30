9246ff0bb955f8ba17fc2389258a18026f51b74d
Indian1999 <haszab99@gmail.com>
2025-05-29 16:30:27 +0200

    Űrhajük felhejezése

--- 2.félév/SpaceWar/main.py
+++ 2.félév/SpaceWar/main.py
@@ -8,6 +8,9 @@ import random
 WIDTH = 900
 HEIGHT = 500
 FPS = 60 # Frames per Second
+SPACESHIP_WIDTH = 80
+SPACESHIP_HEIGHT = 80
+VELOCITY = 5 # Az  úrhajók sebessége (pixel per frame)
 
 clock = pygame.time.Clock() # példányosítjuk a Clock osztályt
 
@@ -19,6 +22,23 @@ ASSETS_PATH = os.path.dirname(os.path.abspath(__file__))
 ASSETS_PATH = os.path.join(ASSETS_PATH, "Assets")
 
 BACKGROUND = pygame.image.load(os.path.join(ASSETS_PATH, "space.png"))
+BACKGROUND = pygame.transform.scale(BACKGROUND, (WIDTH, HEIGHT))
+
+RED_SPACESHIP = pygame.image.load(os.path.join(ASSETS_PATH, "spaceship_red.png"))
+RED_SPACESHIP = pygame.transform.scale(RED_SPACESHIP, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
+RED_SPACESHIP = pygame.transform.rotate(RED_SPACESHIP, 90)
+
+YELLOW_SPACESHIP = pygame.image.load(os.path.join(ASSETS_PATH, "spaceship_yellow.png"))
+YELLOW_SPACESHIP = pygame.transform.scale(YELLOW_SPACESHIP, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
+YELLOW_SPACESHIP = pygame.transform.rotate(YELLOW_SPACESHIP, 270)
+
+def draw_frame():
+    window.blit(BACKGROUND, (0,0))
+    
+    window.blit(RED_SPACESHIP, (675, 250))
+    window.blit(YELLOW_SPACESHIP, (225, 250))
+    
+    pygame.display.update() # frissíti a megjelenítést
 
 def main():
     while True:
@@ -27,6 +47,8 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
+        
+        draw_frame()
         
 # Ez a program belépési pontja
 if __name__ == "__main__":
